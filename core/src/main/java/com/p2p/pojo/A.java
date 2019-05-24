9fc042d49a2b417f158a33f2d7fd439d9a18ba7d
fengh <fenghuo0203@outlook.com>
2019-05-24 17:52:39 +0800

    v0.2

---
+++ core/src/main/java/com/p2p/pojo/A.java
@@ -0,0 +1,8 @@
+package com.p2p.pojo;
+
+/**
+ * @author 李畅
+ * @date 2019/5/24
+ */
+public class A {
+}
---
+++ mgrsite/src/main/java/com/p2p/controller/B.java
@@ -0,0 +1,8 @@
+package com.p2p.controller;
+
+/**
+ * @author 李畅
+ * @date 2019/5/24
+ */
+public class B {
+}
